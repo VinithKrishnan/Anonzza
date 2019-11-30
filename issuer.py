5da744f0796b98b4c65c9306442357854e585d38
Anant Kandikuppa <anantk3@illinois.edu>
2019-11-30 12:02:55 -0600

    Initial setup

---
+++ issuer.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_restplus import Resource,Api
+
+app = Flask(__name__)
+api = Api(app)
+
+@api.route('/currentAccumulator')
+class CurrentAccumulator(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
